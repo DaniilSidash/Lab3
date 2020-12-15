8ad557b8db085004219823de58c360aea319250d
DaniilSidash <70960703+DaniilSidash@users.noreply.github.com>
2020-12-15 18:11:00 +0300

    Update Лаб №3 индивиудальное №3.py

--- Лаб №3 индивиудальное №3.py
+++ Лаб №3 индивиудальное №3.py
@@ -1,4 +1,11 @@
-for i in range(1,10):
-    for j in range(1,10):
-        print("%4d" % (i*j), end='')
-    print()
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Составьте программу, которая печатает таблицу умножения натуральных чисел в
+# десятичной системе счисления
+
+if __name__ == '__main__':
+    for i in range(1, 10):
+        for j in range(1, 10):
+            print("%4d" % (i * j), end='')
+        print()
